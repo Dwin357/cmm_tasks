977f1a5bef6c0acd7831cf8388f66d22813ee4b6
dwin357 <e.r.steinmetz@gmail.com>
2016-05-02 16:10:35 -0500

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,62 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.delete_all if User.any?
+Customer.delete_all if Customer.any?
+Project.delete_all if Project.any?
+Task.delete_all if Task.any?
+TaskEntry.delete_all if TaskEntry.any?
+
+batman = 
+  User.create!(username: "Batman",
+              password: "joker",
+              email: "bruce@waynetech.com")
+
+wayne_tech = 
+  Customer.create!(company: "Wayne Tech",
+                  address: "141 W Jackson",
+                  city: "Gotham",
+                  state: "IL",
+                  zip: "60604")
+
+daily_planet = 
+  Customer.create!(company: "Daily Planet",
+                  address: "350 5th Ave",
+                  city: "Metropolis",
+                  state: "NY",
+                  zip: "10118")
+
+3.times do |n|
+  project = 
+    Project.create!(project_name: "project #{n} for customer planet",
+                    customer: daily_planet)
+
+  2.times do |m|
+    task = 
+      Task.create!(task_name: "task #{m} of prj #{n} for customer planet",
+                   project: project,
+                   user: batman)
+
+    2.times do |o|
+      TaskEntry.create!(note: "line #{o} of task #{m} of prj #{n} for customer planet",
+                        duration: 100,
+                        task: task)
+    end
+  end
+end
+
+3.times do |n|
+  project = 
+    Project.create!(project_name: "project #{n} for customer wayne tech",
+                    customer: wayne_tech)
+
+  2.times do |m|
+    task = 
+      Task.create!(task_name: "task #{m} of prj #{n} for customer wayne tech",
+                   project: project,
+                   user: batman)
+
+    2.times do |o|
+      TaskEntry.create!(note: "line #{o} of task #{m} of prj #{n} for customer wayne tech",
+                        duration: 100,
+                        task: task)
+    end
+  end
+end
